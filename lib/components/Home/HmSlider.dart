94d604ce0f79ac39620811d08cb9d3f4b9b490a2
Zhumeng223 <cuixin2236@163.com>
2026-07-05 15:29:29 +0800

    轮播图的搜索框和指示灯导航

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,8 +11,13 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  final CarouselSliderController _controller = CarouselSliderController();
+
+  int _currentIndex = 0;
+
   Widget _getSlider() {
     return CarouselSlider(
+      carouselController: _controller,
       items: List.generate(widget.bannerList.length, (index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
@@ -22,8 +27,10 @@ class _HmSliderState extends State<HmSlider> {
       }),
       options: CarouselOptions(
         viewportFraction: 1,
-        autoPlay: true,
-        autoPlayInterval: Duration(seconds: 1),
+        onPageChanged: (index, reason) {
+          _currentIndex = index;
+          setState(() {});
+        },
       ),
     );
   }
@@ -52,8 +59,43 @@ class _HmSliderState extends State<HmSlider> {
     );
   }
 
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.animateToPage(index);
+                setState(() {});
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 6,
+                width: _currentIndex == index ? 40 : 6,
+                margin: EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: index == _currentIndex
+                      ? Colors.white
+                      : Color.fromRGBO(0, 0, 0, 0.3),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _getSlider();
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
