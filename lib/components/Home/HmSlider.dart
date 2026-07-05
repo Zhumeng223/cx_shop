c6f52ee9f3401929a0df809bc816ed2c1cc18c82
Zhumeng223 <cuixin2236@163.com>
2026-07-05 15:01:46 +0800

    实现轮播图效果渲染

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,20 +1,35 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  const HmSlider({super.key});
+  final List<BannerItem> bannerList;
+  const HmSlider({super.key, required this.bannerList});
 
   @override
   State<HmSlider> createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider() {
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: MediaQuery.of(context).size.width,
+        );
+      }),
+      options: CarouselOptions(
+        viewportFraction: 1,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 1),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      height: 250,
-      alignment: Alignment.center,
-      child: Text("轮播图", style: TextStyle(color: Colors.white, fontSize: 20)),
-    );
+    return _getSlider();
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/Home/HmHot.dart';
 import 'package:hm_shop/components/Home/HmMoreList.dart';
 import 'package:hm_shop/components/Home/HmSlider.dart';
 import 'package:hm_shop/components/Home/HmSuggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,9 +14,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: "1",
+      imgUrl:
+          "https://img1.baidu.com/it/u=1058602296,448067835&fm=253&app=138&f=JPEG?w=800&h=1422",
+    ),
+    BannerItem(
+      id: "1",
+      imgUrl:
+          "https://pic.rmb.bdstatic.com/bjh/bc1918969db/251013/119df5a840d46b97ffc893d1b15971fe.png",
+    ),
+    BannerItem(
+      id: "1",
+      imgUrl:
+          "https://img0.baidu.com/it/u=248783132,1382637838&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=889",
+    ),
+  ];
+
   List<Widget> _getScrollChildren() {
     return [
-      SliverToBoxAdapter(child: HmSlider()),
+      SliverToBoxAdapter(child: HmSlider(bannerList: _bannerList)),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       SliverToBoxAdapter(child: HmCategory()),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
