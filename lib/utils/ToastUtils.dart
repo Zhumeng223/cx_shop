45b4087aaf77d51383db34660185f8085f0dad7a
Zhumeng223 <cuixin2236@163.com>
2026-07-07 17:07:45 +0800

    登录页面的搭建和表单校验

--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Toastutils {
+  static bool showLoading = false;
   static void showToast(BuildContext context, String msg) {
+    if (showLoading) {
+      return;
+    }
+    showLoading = true;
+    Future.delayed(Duration(seconds: 3), () {
+      showLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(msg, textAlign: TextAlign.center),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusGeometry.circular(40),
         ),
-        width: 120,
+        width: 180,
         behavior: SnackBarBehavior.floating,
       ),
     );
